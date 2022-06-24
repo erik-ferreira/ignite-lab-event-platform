289afc6889a88dd1f3d392cff44529c8c6e1bb82
erik-ferreira <erikdesouzagf@gmail.com>
2022-06-24 18:37:09 -0300

    first class finished

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: number;
+  title: string;
+}
+
+function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
+
+  return (
+    <ul>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default App;

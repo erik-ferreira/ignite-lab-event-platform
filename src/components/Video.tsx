f51499f6f6419bb4b9ed628be465446873585c0d
erik-ferreira <erikdesouzagf@gmail.com>
2022-06-26 19:12:53 -0300

    complete layout video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,87 @@
-// import { Container } from './styles'
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { CaretRight, FileArrowDown, Image } from "phosphor-react";
+
+import { ButtonLink } from "./ButtonLink";
+import { TeacherProfile } from "./TeacherProfile";
+
+import "@vime/core/themes/default.css";
 
 export function Video() {
-  return <div className="flex-1">Video</div>;
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId="SO4-izct7Mc" />
+            <DefaultUi />
+          </Player>
+        </div>
+      </div>
+
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
+              Aula 01 - Abertura do Ignite Lab
+            </h1>
+
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Nessa aula vamos dar início ao projeto criando a estrutura base da
+              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
+              realizar o setup do nosso projeto no GraphCMS criando as entidades
+              da aplicação e integrando a API GraphQL gerada pela plataforma no
+              nosso front-end utilizando Apollo Client.
+            </p>
+
+            <TeacherProfile />
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <ButtonLink variant="primary" />
+            <ButtonLink variant="secondary" />
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material Complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <Image size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }

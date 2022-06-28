bb146b4fd52338041ea4b90b3a5887da4a4c3558
erik-ferreira <erikdesouzagf@gmail.com>
2022-06-28 00:45:57 -0300

    third class finished

--- src/components/TeacherProfile.tsx
+++ src/components/TeacherProfile.tsx
@@ -1,15 +1,21 @@
-export function TeacherProfile() {
+interface TeacherProfileProps {
+  teacher: { bio: string; avatarURL: string; name: string };
+}
+
+export function TeacherProfile(data: TeacherProfileProps) {
   return (
     <div className="flex items-center gap-4 mt-6">
       <img
-        src="https://github.com/erik-ferreira.png"
+        src={data.teacher.avatarURL}
         alt=""
         className="w-16 h-16 rounded-full border-2 border-blue-500"
       />
 
       <div className="leading-relaxed">
-        <strong className="block font-bold text-2xl">Erik Ferreira</strong>
-        <span className="block text-sm text-gray-200"> Dev</span>
+        <strong className="block font-bold text-2xl">
+          {data.teacher.name}
+        </strong>
+        <span className="block text-sm text-gray-200">{data.teacher.bio}</span>
       </div>
     </div>
   );
